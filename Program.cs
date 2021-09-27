26d5f243d117c9e51d1b6b806a2c31c5e243132e
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-09-27 14:26:26 -0300

    coloando driver mysql

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace console_treinamento
 {
     public class Program
     {
-        public static readonly string SqlCNN = "Server=localhost;database=aula_xp;user=root;password=root";
+        public static readonly string SqlCNN = "Server=localhost;database=aula_xp;user=root;password=root;SslMode=none";
 
         static void Main(string[] args)
         {
